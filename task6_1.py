6a9ed05e367156acd549da07492eaf501d2716ca
Volodymyr Pryjmak <v_pryjmak@ukr.net>
2025-03-16 22:16:43 +0200

    Add files via upload

--- task6_1.py
+++ task6_1.py
@@ -1,4 +1,19 @@
 from collections import UserDict
+from functools import wraps
+
+def input_error(func):
+    @wraps(func)
+    def inner(self, field, new_field = ""):
+        try:
+            if func.__name__ == "add_phone":
+               return func(self, field)
+            elif func.__name__ == "edit_phone":    
+               return func(self, field, new_field)
+
+        except ValueError:
+            print(f" Неправильний формат номеру телефону  {field} {new_field} {func.__name__}")
+            return 
+    return inner
 
 class Field:
     def __init__(self, value):
@@ -8,76 +23,84 @@ class Field:
         return str(self.value)
 
 class Name(Field):
-    def init(self,name):
+    def __init__(self,name):
         super().__init__(name)
 
-
 class Phone(Field):
-    def init(self,phone):
+    def __init__(self,phone):
         super().__init__(phone)
+        self.is_valid = self.is_valid()
 
-    def valid(self,phone):
-        if len(phone) == 10:
-           return phone.isdigit() 
-        return False 
+    def is_valid(self):
+        if (len(self.value) != 10) or (self.value.isdigit() == False):
+           return False
+        else:
+            return True 
 
 class Record:
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
 
+    @input_error
     def add_phone(self, phone):
-        if Phone.valid(self, phone) == True: 
-           self.phones.append(phone) 
+        if Phone(phone).is_valid == True:
+           self.phones.append(Phone(phone))
         else:
-           print(f"Некоректний номер телефону: {phone} ")
-         
+            raise ValueError()
+
     def remove_phone(self, phone):
         for el in self.phones: 
-            if el == phone:
+            if str(el) == phone:
                self.phones.remove(el) 
 
-    def edit_phone(self, phone, new_phone):
-        if Phone.valid(self, new_phone) == True: 
-            for el in self.phones:
-                if el == phone:
-                   self.phones[self.phones.index(el)] = new_phone
-                   return
-        else:       
-            print(f"Некоректний новий номер телефону: {new_phone} ")
-            return
-    
-    def __str__(self):
-        return f"Contact name: {self.name}, phones: {'; '.join(p for p in self.phones)}"
-
     def find_phone(self, phone):
-        for el in self.phones: 
-            if el == phone:
+        for el in self.phones:
+            if str(el) == phone:
                return el  
         return None    
+    
+    @input_error
+    def edit_phone(self, phone, new_phone):
+        try:
+            old_phone = Record.find_phone(self,phone) 
+            if  Phone(new_phone).is_valid == True:
+                self.phones[self.phones.index(old_phone)] = new_phone
+            else:
+                raise ValueError()
+        except:
+            raise ValueError() 
+        
+    def __str__(self):
+        return f"Contact name: {self.name}, phones: {'; '.join(str(p) for p in self.phones)}"
 
 class AddressBook(UserDict):
 
-    def add_record(self, name ): 
-        self.data[name] = name
-        
-    def find(self, name):
-        for el in self:
-            if name.strip() == str(el.name).strip():
-               return el
+    def add_record(self, record): 
+        self.data[record.name.value] = record
+
+    def find(self, record_name):
+        for el in self.data:
+            if record_name == el:
+               el_phone = self.data.get(el)
+               return el_phone
         return None
 
-    def delete(self, name):          
-        for el in self:
-            if name.strip() == str(el.name).strip():
-               self.pop(el)
+    def delete(self, record):          
+        for el in self.data:
+            if record == el:
+               self.data.pop(el)
                return 
         return  
-    
+
     def __str__(self):
         st = ""
-        for x in self:
-            st = st + str(x) + "\n"
+        for x in self.data:
+            x_phone = self.data.get(x)
+            st += str(x) + " phones: "
+            for p in x_phone.phones:
+                st += f" {p}"
+            st += "\n"
         return  st
     
 if __name__ == "__main__":    
@@ -105,7 +128,7 @@ if __name__ == "__main__":
 
     # Пошук конкретного телефону у записі John
     found_phone = john.find_phone("5555555555")
-    print(f"{john.name}: {found_phone}")  # Виведення: John: 5555555555
+    print(f"{john.name}:  {found_phone}")  # Виведення: John: 5555555555
 
     john.remove_phone("7777777777")
 # Видалення запису Jane
